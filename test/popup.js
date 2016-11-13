ede2fce70b2b2fa7ebd9e78ed975763ad8648cb5
Serena <serenajiang@berkeley.edu>
2016-11-12 17:32:24 -0800

    sigh andy

--- test/popup.js
+++ test/popup.js
@@ -5,7 +5,30 @@ chrome.tabs.executeScript( {
 		JSON.stringify({
 			'api_key': 'ae63f26c7f98d4b206d13c7779ad104c',
 			'data': selection[0],
-			'threshold': .01
-		})).then(function(res){document.write(res)});
+			'threshold': 0
+		})).then(function(res){
+			var results = $.parseJSON(res).results
+			google.charts.load('current', {'packages':['corechart']});
+      google.charts.setOnLoadCallback(drawChart);
+      function drawChart() {
+
+        var data = google.visualization.arrayToDataTable([
+          ['Task', 'Hours per Day'],
+          ['Work',     11],
+          ['Eat',      2],
+          ['Commute',  2],
+          ['Watch TV', 2],
+          ['Sleep',    7]
+        ]);
+
+        var options = {
+          title: 'My Daily Activities'
+        };
+
+        var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+
+        chart.draw(data, options);
+			// document.write(results)
+		};
 });
 
